54273f13041a3160d37909189a46f5ee71b54523
dhruvbas <dhruv.bas@gmail.com>
2020-04-30 22:16:21 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,372 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "dhruv.bas@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+   
+    $message = $name. " wrote the following:" . "\n\n" . $_POST['message'];
+
+    
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$header);
+    
+    //header('Location: Contact.html'); 
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Advocate Gaurav Thukral</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  
+  <link href="css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="css/resume.min.css" rel="stylesheet">
+  <link href="css/resume.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Gaurav Thukral</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#mission">Mission</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#disclaimer">Disclaimer</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+		<div class = "row">
+			<div class = "col-3">
+				<span class="d-lg-block">
+					<img class="img-fluid img-profile" src="img/Advocate.jpg" alt="" style = "height:100px; width:100px;">
+				</span>
+			</div>
+			<div class = "col-9">
+				<h1 class="mb-0"> Gaurav
+					<span class="text-primary">Thukral</span>
+				</h1>
+				<div class="subheading mb-5">Advocate – Registered with Bar Council of Delhi · +91-7838-33-99-44 ·
+					<a href="mailto:name@email.com">advocate.gaurav.thukral@gmail.com</a>
+				</div>
+			</div>
+		</div>
+		
+        <p class="lead mb-5">Gaurav is a registered member of the Bar Council of Delhi & Delhi High Court Bar Association. Gaurav is actively engaged in research projects related to Alternate Dispute Resolution. In addition, he is also actively involved in Advisory & Consulting, Drafting & Reviewing Agreements, Due Diligence and Non-litigation Support. 
+			He strives to provide a transparent environment for clients and believes in integrity, perfection, creativity & result-oriented approach in order to achieve the best results within a stipulated time.
+		</p>
+		<p class="lead mb-5">Gaurav is deeply involved in research on ADR practices and mediation techniques. He has varied experience in corporate in dealing with resources for issue resolution, policy conflicts and employee grievance handling.
+		A member of ICA (INDIAN COUNCIL OF ARBITRATION) as an arbitrator with more than 15 years’ experience of corporate IT/ITES in HR & Workforce Management.
+		</p>
+        <div class="social-icons">
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="mission">
+      <div class="w-100">
+	  	<div class = "row">
+			<div class = "col-3">
+				<span class="d-lg-block">
+					<img class="img-fluid img-profile" src="img/lawlady.png" alt="" style = "height:100px; width:100px;">
+				</span>
+			</div>
+		    <div class = "col-9">
+				<h2 class="mb-5" style = "margin-top: 16px;">Our Mission</h2>
+			</div>
+		</div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            
+            
+            <p class="lead mb-5">With the goal of empowering the sufferer who is in need of Judicial Remedy or an Alternate Dispute Resolution, we aim to convey this to one and all; <b>Ubi Jus Ibi Remedium </b> which is a basic latin maxim meaning for every wrong, the law provides a remedy.
+				</br></br>
+				At <i><b>apnavakil.org</b></i> our motive is not to run after money or guide for litigation, our motive is simple – to get justice and solution as early as possible with minimal cost and shortest time span.
+				</br></br>
+				This is a kind of initiative to help those in need of justice or legal assistance.
+				</br></br>
+				It could be civil, criminal or anything that can be resolved through law or mediation, conciliation or Arbitration.
+				</br></br>
+				<i><b>We are just not another law firm, we believe in getting you a resolution, justice and peace. </b><i>
+			</p>
+			<p><b>Fedei Defensor</b> is the law firm through which the legal services are processed. </br>
+               The term “Fedei Defensor” is a latin term meaning Defender of faith
+
+          </div>
+        </div>
+	  </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+      <div class="w-100">
+        <h2 class="mb-5">Education</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">Chaudhary Charan Singh University, Meerut</h3>
+            <p>LLM - Criminal Law</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2018 - 2020</span>
+          </div>
+        </div>
+		<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">The Indian Law Institute (ILI), New Delhi</h3>
+            <p>Post Graduate Diploma in Alternative Dispute Resolution (ADR)</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2018 - 2019</span>
+          </div>
+        </div>
+		<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">Chaudhary Charan Singh University, Meerut</h3>
+            <p>Bachelor of Laws - LLB, Law</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2015 - 2018</span>
+          </div>
+        </div>
+		<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">Sikkim Manipal University of Health, Medical and Technological Sciences</h3>
+            <p>MBA, Marketing</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2010 - 2012</span>
+          </div>
+        </div>
+		<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">Indian Institute of Human Rights</h3>
+            <p>Post Graduate Diploma in Human Rights (Master's Degree, Human Rights)</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2006 - 2008</span>
+          </div>
+        </div>
+		<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="subheading mb-3">University of Delhi</h3>
+            <p>Bachelors of Arts, Political Science, History</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2003 - 2006</span>
+          </div>
+        </div>
+
+
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="services">
+      <div class="w-100">
+        <h2 class="mb-5">Services</h2>
+
+        <div class="subheading mb-3">We offer services in</div>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Advisory & Consulting</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Drafting & Reviewing Agreements</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Due Diligence</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Non- litigation Support
+		  </li>
+		  <li>
+            <i class="fa-li fa fa-check"></i>
+            ADR Practices
+		  </li>
+		  <li>
+            <i class="fa-li fa fa-check"></i>
+            Mediation		  </li>
+        </ul>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="contact">
+      <!-- <div class="w-100">
+        <h2 class="mb-5">Contact Us</h2>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-envelope"></i>
+            advocate.gaurav.thukral@gmail.com</li>
+          <li>
+            <i class="fa-li fa fa-mobile-alt"></i>
+            +91-7838-33-99-44</li>
+          <li>
+
+      </div> -->
+	  <div class="w-100">
+		<h2 class="mb-5">Contact Us</h2>
+        <div class="row">
+			
+			<div class = "span8">
+            
+				<div id="sendmessage">We have received your message. Thank you!</div>
+				<div id="errormessage"></div>
+				<form action="" method="post" role="form" class="contactForm">
+				<form action="" method="post" role="form" class="contactForm">
+					<div class="row">
+						<div class="span4 form-group field">
+							<input type="text" name="name" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+							<div class="validation"></div>
+						</div>
+
+						<div class="span4 form-group">
+							<input type="email" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+							<div class="validation"></div>
+						</div>
+						
+						
+						<div class="span8 form-group">
+							<input type="text" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+							<div class="validation"></div>
+						</div>
+						
+						
+						<div class="span8 form-group">
+							<textarea name="message" rows="5" data-rule="required" data-msg="Please write something for us"
+							placeholder="Message"></textarea>
+							<div class="validation"></div>
+							<div class="text-center">
+								<button class="btn btn-theme btn-medium margintop10" type="submit">Send a message</button>
+							</div>
+						</div>
+					</div>
+				  
+				</form>
+			<?php }?>
+			</div>
+			  <div class="span4">
+				<div class="clearfix"></div>
+				<aside class="right-sidebar">
+
+				  <div class="widget">
+					<div class="subheading mb-3">Contact information</div>
+
+					<ul class="fa-ul mb-0">
+					  
+						<li><i class="fa-li fa fa-envelope"></i> advocate.gaurav.thukral@gmail.com</li>
+						<li><i class="fa-li fa fa-mobile-alt"></i>+91-7838-33-99-44</li>
+			  
+					</ul>
+
+				  </div>
+				</aside>
+			  </div>
+        </div>
+	  </div>
+    </section>
+	    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="disclaimer">
+      <div class="w-100">
+	  <h2 class="mb-5">Disclaimer</h2>
+		<p>As per the rules of the Bar Council of India, we are not permitted to solicit work and advertise. Please acknowledge : That there has been no advertisement, personal communication, solicitation, invitation or inducement of any sort whatsoever from us or any of our members to solicit any work through this website; the user wishes to gain more information about us for his/her own information and use; the information about us is provided to the user only on his/her specific request and any information obtained or materials downloaded from this website is completely at the user’s volition and any transmission, receipt or use of this site would not create any lawyer-client relationship. The information provided under this website is solely available at your request for informational purposes only, should not be interpreted as soliciting or advisement. We are not liable for any consequence of any action taken by the user relying on material/information provided under this website. In cases where the user has any legal issues, he/she in all cases must seek independent legal advice.
+		</p>
+
+      </div>
+    </section>
+
+
+	<footer class="footer">
+
+
+		<div class="copyrights">
+			<div class="container" style = "text-align:center">
+				<p>© Copyrights apnavakil.org. All rights reserved.</p>
+			</div>
+		</div>
+
+  </footer>
+    
+
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+</body>
+
+</html>
