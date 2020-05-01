a34418838007fb8da30fa2b9b8ba19d84d86575e
dhruvbas <dhruv.bas@gmail.com>
2020-04-30 21:55:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if(isset($_POST['submit'])){
 		<div class = "row">
 			<div class = "col-3">
 				<span class="d-lg-block">
-					<img class="img-fluid img-profile" src="img/Advocate.jpg" alt="" style = "max-height:100px; max-width:112px;">
+					<img class="img-fluid img-profile" src="img/Advocate.jpg" alt="" style = "height:100px; width:112px;">
 				</span>
 			</div>
 			<div class = "col-9">
@@ -126,7 +126,7 @@ if(isset($_POST['submit'])){
 	  	<div class = "row">
 			<div class = "col-3">
 				<span class="d-lg-block">
-					<img class="img-fluid img-profile" src="img/lawlady.png" alt="" style = "max-height:100px; max-width:120px;">
+					<img class="img-fluid img-profile" src="img/lawlady.png" alt="" style = "height:100px; width:120px;">
 				</span>
 			</div>
 		    <div class = "col-9">
